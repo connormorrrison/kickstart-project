ed89fa0fbce188de241893696816ee43819f56d8
Carson Jan <sc.carson.jan@gmail.com>
2025-11-22 22:03:18 -0800

    app.py api

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import typing
+from typing import List
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+from pydantic import BaseModel
+import datetime
 
 # to host locally:
 # pip install -r requirements.txt
 # fastapi dev app.py 
 # http://localhost:8000
 
+class PostingIn(BaseModel):
+    username: str
+    password_hash: str
+    address: str
+    lat: float
+    lng: float
+    price: float
+    start: List[int]
+    end: List[int]
+
 app = FastAPI()
+load_dotenv()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,6 +42,58 @@ async def ping():
     return {"message": "pong"}
 
 @app.post("/login")
-async def login(username: str, passwordHash: str):
-    return {"message": f"name: {username}, pwd: {passwordHash}"}
+async def login(username: str, password_hash: str):
+    return {"message": f"name: {username}, pwd: {password_hash}"}
 
+@app.post("/register")
+async def register(username: str, password_hash: str, email=None):
+    result = supabase.table("users")\
+    .insert({
+        "username":username,
+        "passwordHash":password_hash,
+        "email":email
+    })\
+    .execute()
+    return result
+
+@app.get("/postings")
+async def postings():
+    return (supabase.table("postings")
+    .select("*")
+    .execute()
+    )
+
+@app.post("/postings/create")
+async def postingsCreate(posting: PostingIn):
+    userId: int = await _getUserId(posting.username, posting.password_hash)
+    if not userId:
+        raise HTTPException(
+            status_code=400,
+            detail="user not found"
+        )
+    
+    return (supabase.table("postings")
+    .insert({
+        "host_id":userId,
+        "address": posting.address,
+        "lat": posting.lat,
+        "lng": posting.lng,
+        "price": posting.price,
+        "start": posting.start,
+        "end": posting.end
+    })
+    .execute())
+
+@app.get("/user/id")
+async def userId(username:str, password_hash: str):
+    return {"id":await _getUserId(username, password_hash)}
+
+async def _getUserId(username:str, passwordHash: str) -> int:
+    response = (supabase.table("users")
+    .select("id")
+    .eq("username", username)
+    .eq("password_hash", passwordHash)
+    .maybe_single()
+    .execute()
+    )
+    return response.data["id"] if response else response
